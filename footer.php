8397152747a62d716bf8644a62f9c6e0336712dc
Infinite Dezigns <info@infinitedezigns.com>
2015-12-22 11:28:59 -0600

    Added files

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Basik
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'basik-1-0' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'basik-1-0' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'basik-1-0' ), 'basik-1-0', '<a href="http://www.infinitedezigns.com" rel="designer">Infinite Dezigns, LLC</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
